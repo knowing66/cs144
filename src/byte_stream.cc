c3905db88b8374c40c230e0da3400dcef49279cf
knowing66 <sy1916444185@gmail.com>
2024-07-12 11:48:05 +0800

    lab0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,65 +1,74 @@
 #include "byte_stream.hh"
+#include <iostream>
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) ,
+  isclosed_(false),pushed_count_(0),poped_count_(0){}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return isclosed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
+  if(isclosed_){
+    cout <<"push data when pipeline is closed"<< endl;
+    return;
+  }
+  int buffer_unused_size=capacity_-buffer_.size();
+  int can_push_size=min(static_cast<int>(data.size()),buffer_unused_size);
+  if(!isclosed_){
+    buffer_+=data.substr(0,can_push_size);
+  }
+  pushed_count_+=can_push_size;
   return;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  isclosed_=true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_-buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return pushed_count_;
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return isclosed_ && pushed_count_==poped_count_;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return poped_count_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  if(buffer_.empty()){
+    return {};
+  }
+  return std::string_view(buffer_);
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  uint64_t bytes_nums_inbuffer=buffer_.size();
+  uint64_t nums_can_pop=min(len,(bytes_nums_inbuffer));
+  buffer_=buffer_.substr(nums_can_pop);
+  poped_count_+=nums_can_pop;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return buffer_.size();
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <deque>
 
 class Reader;
 class Writer;
@@ -25,6 +26,10 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+  std::string buffer_{};
+  bool isclosed_;
+  uint64_t pushed_count_;
+  uint64_t poped_count_;
 };
 
 class Writer : public ByteStream
